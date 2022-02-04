8c8d3b810378145edf2792bd1f5757306b388459
Jacob Needels <44848904+jtneedels@users.noreply.github.com>
2022-02-03 16:31:55 -0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,11 @@
+
+c = 299792458 # speed of light m/s
+h = 6.62607004e-34 # Planck's constant m2 kg / s
+kb = 1.38064852e-23 # Boltzmann's constant m2 kg s-2 K-1
+
+g = 9.81 # m/s2
+re = 6.3781e6 # m radius of earth
+
+gamma = 1.4 # specific heat ratio
+R = 287 # specific gas constant - air\
+H = 8.4e3 # scale height m
